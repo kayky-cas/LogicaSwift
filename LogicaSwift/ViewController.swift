2c1356ed1f8e3e6e34ca7b3bad1c0d930bdcae1b
Kayky Casagrande <kayky-cas@github.com>
2022-12-26 09:28:49 -0300

    Ihhh

--- LogicaSwift/ViewController.swift
+++ LogicaSwift/ViewController.swift
@@ -9,11 +9,33 @@ import UIKit
 
 class ViewController: UIViewController {
 
+	
+	@IBOutlet weak var nameLabel: UILabel!
+	
+	let arr = [
+		"Arthur Miguel",
+		"Davi Luiz",
+		"João Guilherme",
+		"João Vitor",
+		"João Guilherme",
+		"Pedro Miguel",
+		"Pedro Henrique"
+	]
+	
+	var pos = 0
+	
 	override func viewDidLoad() {
 		super.viewDidLoad()
 		// Do any additional setup after loading the view.
+		
+		nameLabel.text = arr[pos]
 	}
 
 
+	@IBAction func touchUpInsideNextButton(_ sender: Any) {
+		pos += 1
+		
+		nameLabel.text = arr[pos % arr.count]
+	}
 }
 
