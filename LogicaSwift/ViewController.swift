1c9856f2fd4748c88a33c450ca8917a8ef702443
Kayky Casagrande <kayky-cas@github.com>
2022-12-26 09:20:22 -0300

    Initial Commit

---
+++ LogicaSwift/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LogicaSwift
+//
+//  Created by Kayky Casagrande on 26/12/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
